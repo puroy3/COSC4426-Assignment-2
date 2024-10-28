88adb4a3d2a8373cef0c26622f33ff1ab98769e4
unknown <shahroz.abbas2018@gmail.com>
2024-10-28 15:06:49 -0400

    -Added Throw Sound

--- Assignment 2/Assets/BallBehaviourScript.cs
+++ Assignment 2/Assets/BallBehaviourScript.cs
@@ -6,7 +6,14 @@ public class BallBehaviourScript : MonoBehaviour
 {
     public Rigidbody basketball;  // Assign the basketball Rigidbody in the inspector
     public Transform hoopTarget;  // Assign the hoop's position
-    // variables
+    
+    [Header("------Audio Source------")]
+    [SerializeField] AudioSource ThrowBallSource;
+
+    [Header("------Audio Clip------")]
+    public AudioClip ThrowBallClip;  
+
+    // Variables
     public float deltaTime = 0;
 
     // Start is called before the first frame update
@@ -23,24 +30,27 @@ public class BallBehaviourScript : MonoBehaviour
 
     void ThrowBall()
     {
+        // Use ThrowBallSource instead of musicSource
+        ThrowBallSource.clip = ThrowBallClip; 
+        ThrowBallSource.Play();
+        
         deltaTime += Time.deltaTime;
         float duration = 0.9f;
         float t01 = deltaTime / duration;
 
-        // move to target
+        // Move to target
         Vector3 A = basketball.position;
         Vector3 B = hoopTarget.position;
         Vector3 pos = Vector3.Lerp(A, B, t01);
 
-        // move in arc
+        // Move in arc
         Vector3 arc = Vector3.up * 5 * Mathf.Sin(t01 * 3.14f);
 
         basketball.position = pos + arc;
 
-        // moment when ball arrives at the target
+        // Moment when ball arrives at the target
         if (t01 >= 1) {
             basketball.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
-
 }
